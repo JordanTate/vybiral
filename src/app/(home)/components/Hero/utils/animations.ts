67e451ad2bb2a57de9d2b265d415544ac50dcfc5
JordanTate <JordanTateAdds@iCloud.com>
2025-12-07 12:50:46 +0000

    Update animation duration

--- src/app/(home)/components/Hero/utils/animations.ts
+++ src/app/(home)/components/Hero/utils/animations.ts
@@ -4,19 +4,19 @@ export const animations = {
   title: {
     initial: { y: 20, opacity: 0, letterSpacing: '0.2em' },
     animate: { y: 0, opacity: 1, letterSpacing: '0.3em' },
-    transition: { duration: 2, ease: 'easeInOut' },
+    transition: { duration: 1, ease: 'easeInOut' },
   },
   subtitle: {
     initial: { x: 20, opacity: 0, filter: 'blur(10px)', scale: 0.95 },
     animate: { x: 0, opacity: 1, filter: 'blur(0px)', scale: 1 },
-    transition: { duration: 2, ease: 'easeInOut' },
+    transition: { duration: 1, ease: 'easeInOut' },
   },
   buttons: {
     listen: {
       initial: { y: 30, opacity: 0, filter: 'blur(10px)' },
       animate: { y: 0, opacity: 1, filter: 'blur(0px)' },
       transition: {
-        duration: 1.2,
+        duration: 0.8,
         ease: 'easeOut',
         delay: 0.8,
       },
@@ -25,9 +25,9 @@ export const animations = {
       initial: { y: 30, opacity: 0, filter: 'blur(10px)' },
       animate: { y: 0, opacity: 1, filter: 'blur(0px)' },
       transition: {
-        duration: 1.2,
+        duration: 0.5,
         ease: 'easeOut',
-        delay: 1.0,
+        delay: 0.8,
       },
     },
   },
