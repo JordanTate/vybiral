093e5b70bcf2312e32ed153083767dd2e609246b
JordanTate <JordanTateAdds@iCloud.com>
2026-01-10 16:11:13 +0000

    Add 'contact' link to page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,7 +1,9 @@
+import Link from 'next/link';
 import Hero from './components/Hero';
 import About from './components/About';
 import Discography from './components/Discography';
 import Listen from './components/Listen';
+import styles from './page.module.scss';
 
 export default function Home() {
   return (
@@ -11,6 +13,9 @@ export default function Home() {
         <Listen />
         <About />
         <Discography />
+        <Link href='/contact' className={styles.link}>
+          Get in Touch
+        </Link>
       </main>
     </div>
   );
