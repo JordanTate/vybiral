6e58020b54da7bba9ff4034a726ab81d3815b361
JordanTate <JordanTateAdds@iCloud.com>
2026-01-10 16:12:31 +0000

    Remove 'contact' link from footer component

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -5,9 +5,6 @@ import styles from './index.module.scss';
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <Link href='/contact' className={styles.link}>
-        Get in Touch
-      </Link>
       <div className={styles.copyright}>
         Petr Vybiral <Copyright width={12} height={12} />{' '}
         {new Date().getFullYear()}
